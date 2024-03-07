d75d043ebeee0feef33de00ff103e97d5ca41fb9
Raftouch <rafanelkin@gmail.com>
2024-03-07 12:01:26 +0100

    feat :rocket: zod resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { schema } from './lib/schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-type FormFields = {
-  email: string
-  password: string
-}
+type FormFields = z.infer<typeof schema>
+
+// type FormFields = {
+//   email: string
+//   password: string
+// }
 
 function App() {
   const {
@@ -15,6 +20,7 @@ function App() {
     defaultValues: {
       email: 'email@test.com',
     },
+    resolver: zodResolver(schema),
   })
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
@@ -30,32 +36,34 @@ function App() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
-        {...register('email', {
-          // required: true,
-          required: 'Email is required',
-          // pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-          // validate: (value) => value.includes('@'),
-          validate: (value) => {
-            if (!value.includes('@')) {
-              return 'Email must include @'
-            }
-            return true
-          },
-        })}
+        {...register('email')}
+        // {...register('email', {
+        //   // required: true,
+        //   required: 'Email is required',
+        //   // pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+        //   // validate: (value) => value.includes('@'),
+        //   validate: (value) => {
+        //     if (!value.includes('@')) {
+        //       return 'Email must include @'
+        //     }
+        //     return true
+        //   },
+        // })}
         type="text"
         placeholder="Email"
       />
       {errors.email && <p>{errors.email.message}</p>}
       <input
-        {...register('password', {
-          // required: true,
-          required: 'Password is required',
-          // minLength: 8,
-          minLength: {
-            value: 8,
-            message: 'Password must have at least 8 characters',
-          },
-        })}
+        {...register('password')}
+        // {...register('password', {
+        //   // required: true,
+        //   required: 'Password is required',
+        //   // minLength: 8,
+        //   minLength: {
+        //     value: 8,
+        //     message: 'Password must have at least 8 characters',
+        //   },
+        // })}
         type="password"
         placeholder="Password"
       />
