547f5ae15f37f498d1c36e4af5229b3db161718d
Raftouch <rafanelkin@gmail.com>
2024-03-07 11:22:19 +0100

    feat :goal_net: add error msg for users

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ type FormFields = {
 }
 
 function App() {
-  const { register, handleSubmit } = useForm<FormFields>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>()
 
   const onSubmit: SubmitHandler<FormFields> = (data) => {
     console.log(data)
@@ -16,21 +20,36 @@ function App() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
         {...register('email', {
-          required: true,
+          // required: true,
+          required: 'Email is required',
           // pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-          validate: (value) => value.includes('@'),
+          // validate: (value) => value.includes('@'),
+          validate: (value) => {
+            if (!value.includes('@')) {
+              return 'Email must include @'
+            }
+            return true
+          },
         })}
         type="text"
         placeholder="Email"
       />
+      {errors.email && <p>{errors.email.message}</p>}
       <input
         {...register('password', {
-          required: true,
-          minLength: 8,
+          // required: true,
+          required: 'Password is required',
+          // minLength: 8,
+          minLength: {
+            value: 8,
+            message: 'Password must have at least 8 characters',
+          },
         })}
         type="password"
         placeholder="Password"
       />
+      {errors.password && <p>{errors.password.message}</p>}
+
       <button type="submit">Submit</button>
     </form>
   )
