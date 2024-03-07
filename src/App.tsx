646fbcc40936204fa76bf50b5893af4d2857c15c
Raftouch <rafanelkin@gmail.com>
2024-03-07 11:12:41 +0100

    feat :safety_vest: add validation

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,23 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('email')} type="text" placeholder="Email" />
-      <input {...register('password')} type="password" placeholder="Password" />
+      <input
+        {...register('email', {
+          required: true,
+          // pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+          validate: (value) => value.includes('@'),
+        })}
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        {...register('password', {
+          required: true,
+          minLength: 8,
+        })}
+        type="password"
+        placeholder="Password"
+      />
       <button type="submit">Submit</button>
     </form>
   )
