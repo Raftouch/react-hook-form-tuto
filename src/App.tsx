9f4d6c09961229b2704d0e3ba5d067420bee35f5
Raftouch <rafanelkin@gmail.com>
2024-03-07 10:52:31 +0100

    init :tada: setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <form>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default App
