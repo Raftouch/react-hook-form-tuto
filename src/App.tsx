7f41abf0d41b1d52c66f50ee9ff7e0b6ba46b993
Raftouch <rafanelkin@gmail.com>
2024-03-07 11:04:23 +0100

    feat :sparkles: add submit logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormFields = {
+  email: string
+  password: string
+}
+
 function App() {
+  const { register, handleSubmit } = useForm<FormFields>()
+
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data)
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('email')} type="text" placeholder="Email" />
+      <input {...register('password')} type="password" placeholder="Password" />
       <button type="submit">Submit</button>
     </form>
   )
